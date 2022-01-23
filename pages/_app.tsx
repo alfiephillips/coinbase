3b5404e72d0480a73ab5a8ffbe3841b21a470e89
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2022-01-23 20:04:20 +0000

    feat: web 3 provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { ThirdwebWeb3Provider as WebProvider } from "@3rdweb/hooks";
+
+const supportedChainIds = [4];
+const connectors = {
+  injected: {},
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WebProvider supportedChainIds={supportedChainIds} connectors={connectors}>
+      <Component {...pageProps} />
+    </WebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
